9c75bd94a1ae52c0c2e818f25bd0c1a8c5fc022d
marinascaminos <mary0caminos@gmail.com>
2022-06-01 20:39:51 -0300

    Created One Page

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show')
+})
+
+document.documentElement.style.setProperty('--animate-duration', '5s');
+
+
+const inputName = document.getElementById('name')
+const button = document.getElementById('validate')
+button.addEventListener('click', )
+
+function validate(){
+    validateName(inputName.value)
+}
+
+function validateName(name){
+
+}
